19bdb3b8f1a11c08ac8ffb4900e818703350791d
MohammedBasha <me@mohammedbasha.com>
2025-07-06 22:25:53 +0300

    Adding the Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import About from "./pages/About.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+          <Header />
           <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/sign-in' element={<SignIn />} />
