136b82d9fed9cd487bf36e5abd1e9942b2d12683
MohammedBasha <me@mohammedbasha.com>
2025-07-06 21:15:18 +0300

    Adding the routes and its corresponding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Profile from "./pages/Profile.jsx";
+
 function App() {
   return (
     <>
-      <div className="text-red-500">
-        Application is under construction.
-      </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/sign-in' element={<SignIn />} />
+              <Route path='/sign-up' element={<SignUp />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/profile' element={<Profile />} />
+          </Routes>
+      </BrowserRouter>
     </>
   )
 }
