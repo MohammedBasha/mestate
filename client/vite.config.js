9fccba63388f602219abbadc7982423f12789c7c
MohammedBasha <me@mohammedbasha.com>
2025-07-12 19:53:52 +0300

    Create the sign-up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,15 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+
   plugins: [
     react(),
     tailwindcss(),
