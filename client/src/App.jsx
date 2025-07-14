6b05844ee9d5d1c169785ea675d6ed85501e8786
MohammedBasha <me@mohammedbasha.com>
2025-07-14 06:54:04 +0300

    Protect the profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
 import Header from "./components/Header.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
               <Route path='/sign-in' element={<SignIn />} />
               <Route path='/sign-up' element={<SignUp />} />
               <Route path='/about' element={<About />} />
-              <Route path='/profile' element={<Profile />} />
+              <Route element={<PrivateRoute />}>
+                  <Route path='/profile' element={<Profile />} />
+              </Route>
           </Routes>
       </BrowserRouter>
     </>
