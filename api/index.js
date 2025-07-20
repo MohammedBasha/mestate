5cb2261b188fe7693d4e4e4c39314d78590f83bb
MohammedBasha <me@mohammedbasha.com>
2025-07-20 07:17:30 +0300

    Adding the build configurations

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from 'cookie-parser';
 import listingRouter from "./routes/listing.route.js";
+import path from 'path';
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
   console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,6 +32,12 @@ app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.status || 500;
